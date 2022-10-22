d273705e6c9d5dc45365e600c164abbe2d25b362
fgkdgkg <116160403+fgkdgkg@users.noreply.github.com>
2022-10-22 18:09:51 +0700

    Create cVariable.c

---
+++ cVariable.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+  int myNum = 5;             // Integer (whole number)
+  float myFloatNum = 5.99;   // Floating point number
+  char myLetter = 'D';       // Character
+
+  printf("%d\n", myNum);
+  printf("%f\n", myFloatNum);
+  printf("%c\n", myLetter);
+  return 0;
+}
